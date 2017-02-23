b8755bcb912fc058fa877b4a6c7c50bd1a53c449
Nichole <nlasater@ucsc.edu>
2017-02-23 14:18:41 -0800

    added functions and fields

--- headers/KnowledgeBase.h
+++ headers/KnowledgeBase.h
@@ -2,7 +2,14 @@
 #include "InfoBase.h"
 
 class KnowledgeBase: public InfoBase{
+	private:
+		unordered_map< string, vector<string> > fact_map;
 	public:
 		KnowledgeBase();
 		~KnowledgeBase();
+		vector<string> lookup(string name);
+		boolean check(string name);
+		void add(string name, vector<string> targets);
+		void remove(string name);
+
 };
