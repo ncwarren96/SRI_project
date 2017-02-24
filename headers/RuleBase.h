abd007fee79e86c013c214e942fb4901346152e2
mcread29 <emilyreed@eduroam-169-233-193-53.ucsc.edu>
2017-02-23 17:35:58 -0800

    Function implementation for rule base

--- headers/RuleBase.h
+++ headers/RuleBase.h
@@ -6,5 +6,10 @@ class RuleBase: public InfoBase{
 		// Constructors and Destructors -----------------------------
 		RuleBase();
 		~RuleBase();
-		
+		// Access Functions ------------------------------------------
+		virtual vector< vector<string> > lookup(string name);
+		virtual bool check(string name);
+		// Manipulation Functions ------------------------------------
+		virtual void add(string name, vector<string> targets);
+		virtual void remove(string name);
 };
--- sources/RuleBase.cpp
+++ sources/RuleBase.cpp
@@ -7,3 +7,19 @@ RuleBase::RuleBase(){
 RuleBase::~RuleBase(){
 	cout<<"destructed RuleBase\n";
 }
+
+vector< vector<string> > RuleBase::lookup(string name){
+	return vector<vector<string>>();
+}
+
+bool RuleBase::check(string name){
+	return true;
+}
+
+void RuleBase::add(string name, vector<string> targets){
+
+}
+
+void RuleBase::remove(string name){
+
+}
