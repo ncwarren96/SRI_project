42dfb881d5e6f8b39eaea935b6ed5df650ae6d9c
Nichole <nlasater@ucsc.edu>
2017-02-23 17:25:54 -0800

    basic header to fix make error

--- headers/RuleBase.h
+++ headers/RuleBase.h
@@ -2,16 +2,9 @@
 #include "InfoBase.h"
 
 class RuleBase: public InfoBase{
-	private:
-		unordered_map< string, vector< vector<string> > > rule_map;
 	public:
 		// Constructors and Destructors -----------------------------
 		RuleBase();
 		~RuleBase();
-		// Access Functions ------------------------------------------
-		vector< vector<string> > lookup(string name);	// returns vector where first entry is operation and following are targets
-		bool check(string name);			// true if rule with key "name" exists
-		// Manipulation Functions ------------------------------------
-		void add(string name, vector<string> targets);	// adds new rule w/ key "name"; if rule "name" already exists and targets is not a duplicate, appends targets. 
-		void remove(string name);	// removes rule "name"
+		
 };
