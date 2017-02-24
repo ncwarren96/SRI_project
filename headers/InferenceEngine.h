1e1f531874dedfeeffef696d1a0074c242dc9771
Nick Warren <ncwarren@ucsc.edu>
2017-02-23 17:42:06 -0800

    edited gitignore

--- headers/InferenceEngine.h
+++ headers/InferenceEngine.h
@@ -1,7 +1,15 @@
 #include "common.h"
+#include "KnowledgeBase.h"
+
 
 class InferenceEngine{
+	private:
+		KnowledgeBase * kb;
+		//RuleBase * rb;
+	
 	public:
 		InferenceEngine();
 		~InferenceEngine();
+		
+		void add(vector<string> line);
 };
--- sources/InferenceEngine.cpp
+++ sources/InferenceEngine.cpp
@@ -2,8 +2,22 @@
 
 InferenceEngine::InferenceEngine(){
 	std::cout<<"construct InferenceEngine\n";
+	kb = new KnowledgeBase();
 }
 
 InferenceEngine::~InferenceEngine(){
 	std::cout<<"destruct InferenceEngine\n";
+}
+
+void InferenceEngine::add(vector<string> line){
+	string type = line[0];
+	line.erase(line.begin());
+	
+	if(type == "fact") {
+		
+		//kb->add(line);//add fact
+		
+	}else if(type == "rule"){
+		//add rule
+	}
 }
