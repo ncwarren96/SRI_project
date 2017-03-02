6fcbe57d0d3330c4895bc84b0f5e9631b0fc888a
Nichole <nlasater@ucsc.edu>
2017-03-01 18:40:45 -0800

    removed commented out sections

--- sources/RuleBase.cpp
+++ sources/RuleBase.cpp
@@ -32,23 +32,6 @@ void RuleBase::add(map<string, vector<string>> data){
 	rule_map[name].push_back(data);
 }
 
-
-/*void RuleBase::add(vector<string> data){
-	string name = data.front();	// store first element as name
-	data.erase(data.begin());	// delete first element
-	cout<<"Adding rule: "<<name<<endl;
-	vector< vector<string> > container;	// create 2D vector for emplace()
-	container.push_back(data);	// add data to the 2D vector container
-	auto wasAdded = rule_map.emplace(name, container);	// attempt to create new map entry w/ key "name"
-	
-	if( wasAdded.second == false ) {	//if emplace failed due to duplicate key
-		// check to be sure it's not a duplicate:
-		if ( (std::find((*wasAdded.first).second.begin(), (*wasAdded.first).second.end(), data) == (*wasAdded.first).second.end())) {
-			(*wasAdded.first).second.push_back(data);	// if not, add targets to the vector at key "name" 
-		}
-	}
-}*/
-
 void RuleBase::removeAll(string name){
 	rule_map.erase(name);
 }
@@ -60,7 +43,6 @@ void RuleBase::remove(map<string, vector<string>> data) {
 	auto it = std::find(rule_map.at(temp[0]).begin(), rule_map.at(temp[0]).end(), data);
 	// if found:
 	if ( it != rule_map.at(temp[0]).end()) {
-		//std::cout << "found duplicate";
 		rule_map.at(temp[0]).erase(it); // erase item
 	
 	}
@@ -74,13 +56,6 @@ vector<map<string, vector<string>>> RuleBase::getRules(){
 			ret.push_back(rule);
 		}
 	}
-	/*for(pair<string, vector<vector<string>>> element : rule_map ){
-		for(int i=0; i<element.second.size(); i++){
-			vector<string> rule = element.second[i];
-			rule.insert(rule.begin(), element.first);
-			ret.push_back(rule);
-		}
-	}*/
 	return ret;
 }
 
