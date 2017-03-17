fc0dab061a9d50aedae24044d61058c803fcbd94
Mason Reed <machreed@ucsc.edu>
2017-03-16 18:02:58 -0700

    Header mistake

--- headers/InferenceEngine.h
+++ headers/InferenceEngine.h
@@ -6,7 +6,7 @@ class Parser;
 
 class InferenceEngine{
 	private:
-		typedef void (InferenceEngine::*InEnMFP)(string);
+		typedef string (InferenceEngine::*InEnMFP)(string);
 		map<string, InEnMFP> funcMap;	// function pointers
 		KnowledgeBase * kb;
 		RuleBase * rb;
@@ -19,18 +19,18 @@ class InferenceEngine{
 		InferenceEngine();
 		~InferenceEngine();
 		
-		void processLine(stringstream & p_ss); // processes instructions on line
-		void processFact(string p_string); // processes and adds facts to KB
-		void processRule(string p_string); // processes and adds facts to KB
-		void processLoad(string p_string); // imports a .sri file
-		void processDump(string p_string); // exports a .sri file
+		string processLine(stringstream & p_ss); // processes instructions on line
+		string processFact(string p_string); // processes and adds facts to KB
+		string processRule(string p_string); // processes and adds facts to KB
+		string processLoad(string p_string); // imports a .sri file
+		string processDump(string p_string); // exports a .sri file
 		vector<map<string,string>> inferenceFact(string p_name, vector<string> & p_vars); // perform inference on a fact
 		vector<map<string,string>> inferenceRule(string p_name, vector<string> & p_vars); // perform inference on a rule
 		map<string, vector<string>> findRule(string p_name, int p_size); // locates and returns a rule for inferencing
 		vector<map<string,string>> findAND(vector<vector<map<string,string>>> p_targets); // performs AND operation
 		vector<map<string,string>> findOR (vector<vector<map<string,string>>> p_targets); // performs OR operation
-		void processInference(string p_string); // processes and executes an Inference
-		void processDrop(string p_string); // perform a drop
+		string processInference(string p_string); // processes and executes an Inference
+		string processDrop(string p_string); // perform a drop
 		
 		string genFact(vector<string> p_strings); // generates a string representing a fact
 		string genRule(map<string, vector<string>> p_rule); // generates a string representing a rule
